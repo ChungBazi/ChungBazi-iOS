1179e7d353e627cbf7053dfb08de5664ad71011d
Hyeon-Ju <128601891+dlguszoo@users.noreply.github.com>
2025-01-27 23:21:57 +0900

    Update SceneDelegate.swift

--- ChungBazi/ChungBazi/App/SceneDelegate.swift
+++ ChungBazi/ChungBazi/App/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         
-        let rootVC = AlarmViewController()
+        let rootVC = MainTabBarController()
         let navigationController = UINavigationController(rootViewController: rootVC)
         navigationController.isNavigationBarHidden = true
         
