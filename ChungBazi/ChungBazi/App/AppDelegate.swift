e386483d75178f74c62795eff8df00a150a0e469
Hyeon-Ju <lhjivy1024@gmail.com>
2025-02-05 03:47:18 +0900

    #24 Fix: 카카오앱 로그인시 앱으로 다시 돌아오게 설정 scene/app delegate

--- ChungBazi/ChungBazi/App/AppDelegate.swift
+++ ChungBazi/ChungBazi/App/AppDelegate.swift
@@ -51,6 +51,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        
+        return false
+    }
 
     // MARK: UISceneSession Lifecycle
 
