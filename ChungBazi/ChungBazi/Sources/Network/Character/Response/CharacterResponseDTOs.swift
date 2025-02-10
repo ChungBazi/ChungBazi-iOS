872d65be7fa33b6d998d269f935028c2506ade0b
Hyeon-Ju <lhjivy1024@gmail.com>
2025-02-10 18:37:15 +0900

    #59 Feat: 캐릭터 관련 3개 api 세팅

---
+++ ChungBazi/ChungBazi/Sources/Network/Character/Response/CharacterResponseDTOs.swift
@@ -0,0 +1,22 @@
+//  CharacterResponseDTOs.swift
+//  ChungBazi
+//
+//  Created by 이현주 on 2/10/25.
+//
+
+struct MainCharacterResponseDto: Decodable {
+    let rewardLevel: String
+    let name: String
+    let nextRewardLevel: String
+    let posts: Int
+    let comments: Int
+}
+
+struct CharacterListResponseDto: Decodable {
+    let character: [CharacterInfo]
+}
+
+struct CharacterInfo: Decodable {
+    let rewardLevel: String
+    let open: Bool
+}
